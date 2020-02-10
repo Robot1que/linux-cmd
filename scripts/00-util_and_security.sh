180c2c4417d430c7b5cd8c8f170b2b5593158434
Pavels Ahmadulins <pavels.ahmadulins@outlook.com>
2020-02-10 20:05:27 +0000

    Update 00-util_and_security.sh

--- scripts/00-util_and_security.sh
+++ scripts/00-util_and_security.sh
@@ -5,8 +5,36 @@ whatis # display one-line manual page description
 whereis # locate the binary, source, and manual page files for a command
 
 man # display manual for a command
-
 date # print or set system time
+$? # last command exit code
+cd - # change directory to previous location
+
+# MULTIPLE COMMANDS IN SINGLE LINE
+
+echo "Command 1" && echo "Command 2" # execute the second command if the first one succeeds
+echo "Command 1"; echo "Command 2" # execute the first command, then the second
+
+# STREAMS
+
+echo "content" > ~/file.txt # set content of a file
+echo "content" >> ~/file.txt # append content to a file
+find /etc -name *apache* 2>/dev/null # ignore stderr
+
+read age # promt user to input value
+echo $age
+
+# ALIASES
+
+alias # show registered aliases
+alias i="sudo apt install"
+i tmux
+
+# COMMAND HISTORY
+
+history # show command history
+history -d 100 # delete command from history
+!100 # execute command from history
+sudo !! # execute last command
 
 # TRUST A CERTIFICATE
 
